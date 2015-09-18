4f768b4ba6a8360bb9c2afa7e5f7651a8dd3b1c0
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-09-18 07:16:58 -0400

    adds exception handling with filp/whoops

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,26 @@
 <?php 
+namespace Example;
 
-echo 'Hello, world!';
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+* Register the error handler
+* 
+*/
+
+$whoops = new \Whoops\Run;
+if ($environment !== 'production'){
+	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+	$whoops->pushHandler(function($e){
+		echo 'Friendly error page and send an email';
+	});
+}
+
+$whoops->register();
+
+throw new \Exception;
